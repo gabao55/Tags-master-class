54232cf624954a5aa7f0788a8822b9a181e13eaa
Gabriel Rosin <gabriel.s.rosin@gmail.com>
2022-08-17 11:36:11 -0300

    Adds image pixel

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 function eventoEspecífico() {
     window.lintrk('track', { conversion_id: 10143361 });
 }
+
+function pixelDeImagem() {
+    const body = document.querySelector("body");
+    body.innerHTML += `
+        <img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=4616737&conversionId=10143377&fmt=gif" />
+    `
+}
